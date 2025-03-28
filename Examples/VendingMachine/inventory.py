f38eba0a818418e9830e9166a871b5bc91fc5c78
pranavbrkr <pranavbrkr@gmail.com>
2025-03-27 17:31:48 -0700

    coin notes and state interface

---
+++ Examples/VendingMachine/inventory.py
@@ -0,0 +1,18 @@
+class Inventory:
+  def __init__(self):
+    self.products = {}
+
+  def addProducts(self, product, quantity):
+    self.products[product] = quantity
+
+  def removeProducts(self, product):
+    del self.products[product]
+  
+  def updateQuantity(self, product, quantity):
+    self.products[product] = quantity
+  
+  def getQuantity(self, product):
+    return self.products.get(product, 0)
+  
+  def isAvailable(self, product):
+    return product in self.products and self.products[product] > 0
