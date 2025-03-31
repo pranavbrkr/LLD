3ca7e438a36107c13dc862a20b63c720aef0cb38
pranavbrkr <pranavbrkr@gmail.com>
2025-03-31 12:28:45 -0700

    client

---
+++ Examples/LoggingFramework/client.py
@@ -0,0 +1,7 @@
+from logger import Logger
+
+if __name__ == "__main__":
+  logger = Logger()
+  logger.info("This is an info")
+  logger.debug("This is an debug")
+  logger.error("This is an error")
